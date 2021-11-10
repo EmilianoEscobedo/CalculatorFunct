2ea5f29a1cc65b778809d389b3b656292b243b6e
Laingard <emilianoescobedo9@gmail.com>
2021-11-10 20:32:08 -0300

    builded eval() method as math core and finished button events (def?)

--- GUIproof.py
+++ GUIproof.py
@@ -1,63 +1,74 @@
 import PySimpleGUI as sg
-from Button import *
+from PySimpleGUI.PySimpleGUI import Window
+from math import *
 
 
 def InitialInterface():
-    
+    #Layout                             #Create GUI
     layout= [
-        [sg.Text('0', font='Italic 15', size=(26,1), justification='right', key='screen', background_color=('#000'))],
-        [sg.Button('7',font='Italic 20',size=(3,1),key='7'),
-            sg.Button('8',font='Italic 20',size=(3,1),key='8'),
-            sg.Button('9',font='Italic 20',size=(3,1),key='9'),
-            sg.Button('*',font='Italic 20',size=(3,1),key='*')],
-             [sg.Button('4',font='Italic 20',size=(3,1),key='4'),
-              sg.Button('5',font='Italic 20',size=(3,1),key='5'),
-               sg.Button('6',font='Italic 20',size=(3,1),key='6'),
-               sg.Button('/',font='Italic 20',size=(3,1),key='/')],
-               [sg.Button('1',font='Italic 20',size=(3,1),key='1'),
-              sg.Button('2',font='Italic 20',size=(3,1),key='2'),
-               sg.Button('3',font='Italic 20',size=(3,1),key='3'),
-               sg.Button('-',font='Italic 20',size=(3,1),key='-')],
-               [sg.Button('0',font='Italic 20',size=(3,1),key='0'),
-              sg.Button('.',font='Italic 20',size=(3,1),key='.'),
-               sg.Button('=',font='Italic 20',size=(3,1),key='='),
-               sg.Button('+',font='Italic 20',size=(3,1),key='+')],
-               [sg.Button('Clear',font='Italic 20',size=(3,1),key='clear')]
-               
-    ]
-    exit = [[sg.Button('Exit',font='Italic 20',size=(12,3),key='exit')]]
-    return layout + exit
+        [sg.Text('0', font='Italic 15', size=(29,2), justification='right', key='input', background_color=('#000'))],
+        [sg.Button('+/-',font='Italic 20',size=(3,1),key='+/-'),sg.Button('7',font='Italic 20',size=(3,1),key=7),sg.Button('8',font='Italic 20',size=(3,1),key=8), sg.Button('9',font='Italic 20',size=(3,1),key=9),sg.Button('*',font='Italic 20',size=(3,1),key='*')],
+        [sg.Button('x!',font='Italic 20',size=(3,1),key='x!'),sg.Button('4',font='Italic 20',size=(3,1),key=4),sg.Button('5',font='Italic 20',size=(3,1),key=5),sg.Button('6',font='Italic 20',size=(3,1),key=6),sg.Button('/',font='Italic 20',size=(3,1),key='/')],
+        [sg.Button('xⁿ',font='Italic 20',size=(3,1),key='xⁿ'),sg.Button('1',font='Italic 20',size=(3,1),key=1),sg.Button('2',font='Italic 20',size=(3,1),key=2),sg.Button('3',font='Italic 20',size=(3,1),key=3),sg.Button('-',font='Italic 20',size=(3,1),key='-')],
+        [sg.Button('√',font='Italic 20',size=(3,1),key='√'),sg.Button('0',font='Italic 20',size=(3,1),key=0),sg.Button('.',font='Italic 20',size=(3,1),key='.'),sg.Button('=',font='Italic 20',size=(3,1),key='='),sg.Button('+',font='Italic 20',size=(3,1),key='+')],
+        [sg.Button('(',font='Italic 20',size=(3,1),key='('),sg.Button(')',font='Italic 20',size=(3,1),key=')'), sg.Button('Clear',font='Italic 20',size=(7,1),key='clear'),sg.Button('<<',font='Italic 20',size=(3,1),key='<<')],
+        ]
+    return layout
 
+    # Set PySimpleGUI
 def principal():
-    hight = 500
-    width = 900    
-    body = sg.Window ('',InitialInterface(), size = (width,hight),element_justification='center')
+    hight = 400
+    width = 400   
+    body = sg.Window ('LCS v2.0',InitialInterface(), size = (width,hight),element_justification='center')
     body.Finalize()
-    cad= ''
-    firstOperator=''
-    secondOperator=''
     
+    # Set Operators
+    equal= ''
+    operators=['+','-','*','/','(',')','.']
+    specialOperators=['√','x!','xⁿ','+/-']
+    
+    #Loop GUI
     while True:
-        event, value = body.read()
-        if (event == None or event == 'exit') :
-            break
-        if (event!='exit'):
-            try:
-                if(int(event) or event == '0'):
-                    cad= buttonNumber(event,cad)
-                    firstOperator=int(cad)
-                    body ['screen'].Update(firstOperator)     
-            except:
-                print('operation') 
-            #cad= buttonNumber(event,cad)
-            #ventana ['screen'].Update(cad)
-        if (event=='+'):
-            n1=cad
-            cad=''
-            body ['screen'].Update(cad+n1)
-        if (event=='clear'):
-            cad= ''
-            body['screen'].Update(0)
+        event, values = body.read()
         
+    #Buttons event
+        
+        if (event == None) :
+            break
+        elif (event=='clear'):
+            equal=equal=''
+            body ['input'].Update(equal)
+        elif (event== '<<'):
+            equal=equal[:-1]
+            body ['input'].Update(equal)
+        elif (event in range(0,10) or event in operators):
+            equal+=str(event)
+            body['input'].Update(equal)
+        elif event in specialOperators:
+            if '√' in event :
+                equal=sqrt(float(equal))
+                body['input'].Update(equal)
+            if 'x!' in event :
+                equal=factorial(float(equal))
+                body['input'].Update(equal)
+            if 'xⁿ' in event :
+                equal+='**'
+                body['input'].Update(equal)
+            if '+/-' in event :
+                if '-' in equal[0]:
+                    equal=equal[1 : : ]
+                    body['input'].Update(equal)
+                else:
+                    equal=('-'+equal)
+                    body['input'].Update(equal)
+        elif (event == '=' ):
+            try:
+                body ['input'].Update(equal)
+                equal=str(eval(equal))
+                body ['input'].Update(equal)
+            except:
+                equal=equal=''
+                body ['input'].Update('Math Error') 
+    
     body.Close()
 principal()
